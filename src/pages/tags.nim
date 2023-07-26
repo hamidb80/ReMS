26c4d7cfa740fe0c09fc916b32c175bb8349eb4a
hamidb80 <hr.bolouri@gmail.com>
2023-07-26 16:04:32 +0430

    add icons

--- src/pages/tags.nim
+++ src/pages/tags.nim
@@ -27,10 +27,129 @@ const
   green = ("#cbfbad", "#479417")
   lemon = ("#e6f8a0", "#617900")
 
+  icons  = [
+    "fa-hashtag",
+    "fa-clock",
+    "fa-stopwatch",
+    "fa-calendar",
+    "fa-user",
+    "fa-users",
+    "fa-globe",
+    "fa-heart",
+    "fa-house",
+    "fa-phone",
+    "fa-sun",
+    "fa-moon",
+    "fa-tree",
+    "fa-spa",
+    "fa-seedling",
+    "fa-plane",
+    "fa-paper-plane",
+    "fa-ruler",
+    "fa-poop",
+    "fa-gift",
+    "fa-umbrella",
+    "fa-bookmark",
+    "fa-book",
+    "fa-dna",
+    "fa-life-ring",
+    "fa-mug-saucer",
+    "fa-quote-left",
+    "fa-cookie-bite",
+    "fa-dumbbell",
+    "fa-fish",
+    "fa-shapes",
+    "fa-shield-halved",
+    "fa-note-sticky",
+    "fa-location-crosshairs",
+    "fa-city",
+    "fa-infinity",
+    "fa-skull",
+    "fa-graduation-cap",
+    "fa-question",
+    "fa-info",
+    "fa-cloud",
+    "fa-plug",
+    "fa-pen",
+    "fa-bolt",
+    "fa-check",
+    "fa-xmark",
+    "fa-atom",
+    "fa-ghost",
+    "fa-vial",
+    "fa-flask",
+    "fa-face-laugh",
+    "fa-face-smile",
+    "fa-face-meh",
+    "fa-face-sad-cry",
+    "fa-face-surprise",
+    "fa-face-angry",
+    "fa-robot",
+    "fa-feather",
+    "fa-bed",
+    "fa-code",
+    "fa-filter",
+    "fa-ellipsis",
+    "fa-cube",
+    "fa-circle-nodes",
+    "fa-apple-whole",
+    "fa-florin-sign",
+    "fa-hands-clapping",
+    "fa-hand-fist",
+    "fa-anchor",
+    "fa-microchip",
+    "fa-paperclip",
+    "fa-link",
+    "fa-star",
+    "fa-asterisk",
+    "fa-thumbtack",
+    "fa-person",
+    "fa-palette",
+    "fa-gamepad",
+    "fa-marker",
+    "fa-trophy",
+    "fa-crown",
+    "fa-recycle",
+    "fa-satellite",
+    "fa-satellite-dish",
+    "fa-road-barrier",
+    "fa-ribbon",
+    "fa-mountain",
+    "fa-mosque",
+    "fa-microphone-lines",
+    "fa-lock",
+    "fa-lock-open",
+    "fa-ice-cream",
+    "fa-hourglass-end",
+    "fa-helicopter",
+    "fa-gun",
+    "fa-gem",
+    "fa-fan",
+    "fa-explosion",
+    "fa-couch",
+    "fa-chess-knight",
+    "fa-meteor",
+    "fa-bomb",
+    "fa-triangle-exclamation",
+    "fa-radiation",
+    "fa-comment",
+    "fa-glasses",
+    "fa-lightbulb",
+    "fa-compass",
+    "fa-location-dot",
+    "fa-map-pin",
+    "fa-key",
+    "fa-snowflake",
+    "fa-stairs",
+    "fa-fire",
+    "fa-file-lines",
+    "fa-bell",
+    "fa-filter",]
 
 func tag(name: string, c: ColorTheme, selected: bool): VNode =
   buildHtml:
-    tdiv(class = "badge border-1 solid-border rounded-pill mx-2 my-1 pointer", style = style(
+    tdiv(class = "badge border-1 solid-border rounded-pill mx-2 my-1 pointer",
+      style = style(
       (StyleAttr.background, c.bg.cstring),
       (StyleAttr.color, c.fg.cstring),
       (StyleAttr.borderColor, c.fg.cstring),
@@ -48,33 +167,67 @@ proc createDom: Vnode =
           text "Tags"
 
     tdiv(class = "p-4 mx-4 my-2"):
-      h6(class="mb-3"):
+      h6(class = "mb-3"):
         italic(class = "fa-solid fa-bars-staggered me-2")
         text "All Tags"
 
-      tdiv(class="d-flex flex-row"):
+      tdiv(class = "d-flex flex-row"):
         tag("hey", lemon, true)
         tag("hey", blue, false)
         tag("hey", peach, false)
 
     tdiv(class = "p-4 mx-4 my-2"):
-      h6(class="mb-3"):
+      h6(class = "mb-3"):
         italic(class = "fa-solid fa-gear me-2")
         text "Config"
 
-      # TODO
-      # name
-      # icon - select from a grid list
-      # show name
-      # have value
+      tdiv(class = "form-control"):
+        # name
+        tdiv(class = "form-group d-inline-block mx-2"):
+          label(class = "form-check-label"):
+            text "name: "
 
-      # bg color
-      # fg color
+          input(`type` = "text", class = "form-control tag-input")
 
-      # demo
+        # icon
+        if state == asChooseIcon:
+          tdiv(class = "d-flex flex-row flex-wrap justify-content-between"):
+            for c in icons:
+              tdiv(class = "btn btn-lg btn-outline-dark rounded-2 m-2 p-2"):
+                italic(class = "m-2 fa-solid " & c)
+        else:
+          discard
 
-      if select == none:
-        btn "add"
+        # show name
+        tdiv(class = "form-check form-switch"):
+          input(class = "form-check-input", `type` = "checkbox")
+          label(class = "form-check-label"):
+            text "show name"
+
+        # has value
+        tdiv(class = "form-check form-switch"):
+          input(class = "form-check-input", `type` = "checkbox")
+          label(class = "form-check-label"):
+            text "has value"
+
+        tdiv(class = "form-group d-inline-block mx-2"):
+          label(class = "form-check-label"):
+            text "background color: "
+
+          input(`type` = "color", class = "form-control")
+
+        tdiv(class = "form-group d-inline-block mx-2"):
+          label(class = "form-check-label"):
+            text "foreground color: "
+
+          input(`type` = "color", class = "form-control")
+
+
+      # ------------- demo ----------------
+
+      # if select == none:
+      #   btn "add"
+      # else: "update"
 
 when isMainModule:
   setRenderer createDom
