9bb4507d56bae244821a9e7510fe21ce81ede08c
hamidb80 <hr.bolouri@gmail.com>
2023-08-17 10:36:22 +0330

    add masonry style

--- src/frontend/pages/notes_list.nim
+++ src/frontend/pages/notes_list.nim
@@ -19,23 +19,29 @@ proc fetchNotes =
     notes = cast[typeof notes](r.data)
     redraw()
 
-proc reqNewNote = 
+proc reqNewNote =
   post_api_notes_new_url().postApi.dthen proc(r: AxiosResponse) =
     let id = cast[int64](r.data)
     redirect get_note_editor_url id
 
 
 # ----- UI
-
+# TODO make preview note a indepepndent component / used in board.nim too
 proc notePreviewC(np: NotePreview): VNode =
   buildHtml:
-    tdiv(class="p-4 border rounded m-4 bg-white"):
-      a(href = get_note_editor_url(np.id)):
-        span:
-          text "#"
-          text $np.id
+    tdiv(class = "masonry-item card m-4 border rounded bg-white"):
+      tdiv(class = "card-body"):
+        verbatim deserizalize(compTable, np.preview).innerHtml
+      tdiv(class = "card-footer d-flex justify-content-center"):
+        a(class = "btn mx-1 btn-compact btn-outline-info",
+            href = get_note_editor_url(np.id)):
+          icon "fa-link"
+        a(class = "btn mx-1 btn-compact btn-outline-warning",
+            href = get_note_editor_url(np.id)):
+          icon "fa-pen"
+        button(class = "btn mx-1 btn-compact btn-outline-danger"):
+          icon "fa-close"
 
-      verbatim deserizalize(compTable, np.preview).innerHtml
 
 
 proc createDom: Vnode =
@@ -45,13 +51,15 @@ proc createDom: Vnode =
     nav(class = "navbar navbar-expand-lg bg-white"):
       tdiv(class = "container-fluid"):
         a(class = "navbar-brand", href = "#"):
-          icon("fa-paper fa-xl me-3 ms-1")
+          icon("fa-note-sticky fa-xl me-3 ms-1")
           text "Notes"
 
     tdiv(class = "p-4 m-4"):
-      tdiv(class = "d-flex flex-wrap justify-content-around my-4"):
-        button(class = "p-4 border rounded m-4 bg-white text-primary", onclick=reqNewNote):
-          icon "fa-plus fa-xl"
+      tdiv(class = "masonry-container justify-content-around my-4"):
+        button(
+          class = "masonry-item p-4 m-4 btn btn-outline-primary rounded",
+          onclick = reqNewNote):
+          icon "fa-plus fa-xl my-4"
 
         for n in notes:
           notePreviewC n
