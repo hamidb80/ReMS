3f1e2f2b928910f5f10930c964ce5b840f468806
hamidb80 <hr.bolouri@gmail.com>
2023-10-12 20:51:18 +0330

    fix

--- src/frontend/pages/tags.nim
+++ src/frontend/pages/tags.nim
@@ -25,7 +25,7 @@ const
 var
   state = asInit
   selectedTagI = noIndex
-  currentTag: Tag
+  currentTag = none Tag
   tags: seq[Tag]
   colors: seq[ColorTheme]
 
@@ -45,8 +45,7 @@ proc fetchDefaultPalette: Future[void] =
   newPromise proc(resolve, reject: proc()) =
     apiGetPalette "default", proc(cs: seq[ColorTheme]) =
       colors = cs
-      currentTag.theme = cs[0]
-      currentTag = dummyTag()
+      currentTag = some dummyTag()
       resolve()
 
 proc fetchTags: Future[void] =
@@ -56,17 +55,17 @@ proc fetchTags: Future[void] =
       resolve()
 
 proc onIconSelected(icon: string) =
-  currentTag.icon = icon
+  currentTag.get.icon = icon
   state = asInit
 
 proc genChangeSelectedTagi(i: int): proc() =
   proc =
     if selectedTagI == i:
       selectedTagI = noIndex
-      currentTag = dummyTag()
+      currentTag = some dummyTag()
     else:
       selectedTagI = i
-      currentTag = tags[i]
+      currentTag = some tags[i]
 
 
 proc iconSelectionBLock(icon: string, setIcon: proc(icon: string)): VNode =
@@ -80,147 +79,146 @@ proc iconSelectionBLock(icon: string, setIcon: proc(icon: string)): VNode =
 proc createDom: Vnode =
   result = buildHtml tdiv:
     snackbar()
-
     nav(class = "navbar navbar-expand-lg bg-white"):
       tdiv(class = "container-fluid"):
         a(class = "navbar-brand", href = "#"):
           icon "fa-hashtag fa-xl me-3 ms-1"
           text "Tags"
 
-    tdiv(class = "p-4 mx-4 my-2"):
-      h6(class = "mb-3"):
-        icon "fa-bars-staggered me-2"
-        text "All Tags"
+    if issome currentTag:
+      tdiv(class = "p-4 mx-4 my-2"):
+        h6(class = "mb-3"):
+          icon "fa-bars-staggered me-2"
+          text "All Tags"
 
-      tdiv(class = "d-flex flex-row flex-wrap"):
-        for i, t in tags:
-          tagViewC t, "val", genChangeSelectedTagi i
+        tdiv(class = "d-flex flex-row flex-wrap"):
+          for i, t in tags:
+            tagViewC t, "val", genChangeSelectedTagi i
 
-    tdiv(class = "p-4 mx-4 my-2"):
-      h6(class = "mb-3"):
-        icon "fa-gear me-2"
-        text "Config"
+      tdiv(class = "p-4 mx-4 my-2"):
+        h6(class = "mb-3"):
+          icon "fa-gear me-2"
+          text "Config"
 
-      tdiv(class = "form-control"):
-        if state == asSelectIcon:
-          tdiv(class = "d-flex flex-row flex-wrap justify-content-between"):
-            for c in icons:
-              iconSelectionBLock(c, onIconSelected)
+        tdiv(class = "form-control"):
+          if state == asSelectIcon:
+            tdiv(class = "d-flex flex-row flex-wrap justify-content-between"):
+              for c in icons:
+                iconSelectionBLock(c, onIconSelected)
 
-        else:
-          # name
-          tdiv(class = "form-group d-inline-block mx-2"):
-            label(class = "form-check-label"):
-              text "name: "
+          else:
+            # name
+            tdiv(class = "form-group d-inline-block mx-2"):
+              label(class = "form-check-label"):
+                text "name: "
 
-            input(`type` = "text", class = "form-control tag-input",
-                value = currentTag.name):
-              proc oninput(e: Event, v: Vnode) =
-                currentTag.name = $e.target.value
+              input(`type` = "text", class = "form-control tag-input",
+                  value = currentTag.get.name):
+                proc oninput(e: Event, v: Vnode) =
+                  currentTag.get.name = $e.target.value
 
-          # icon
-          tdiv(class = "form-check"):
-            label(class = "form-check-label"):
-              text "icon: "
+            # icon
+            tdiv(class = "form-check"):
+              label(class = "form-check-label"):
+                text "icon: "
 
-            tdiv(class = "d-inline-block"):
-              tdiv(class = "btn btn-lg btn-outline-dark rounded-2 m-2 p-2"):
-                icon "m-2 " & $currentTag.icon
+              tdiv(class = "d-inline-block"):
+                tdiv(class = "btn btn-lg btn-outline-dark rounded-2 m-2 p-2"):
+                  icon "m-2 " & $currentTag.get.icon
+
+                proc onclick =
+                  state = asSelectIcon
+
+            tdiv(class = "form-check form-switch"):
+              checkbox currentTag.get.is_private, proc (b: bool) =
+                currentTag.get.is_private = b
+
+              label(class = "form-check-label"):
+                text "is private"
+
+            tdiv(class = "form-check form-switch"):
+              checkbox currentTag.get.show_name, proc (b: bool) =
+                currentTag.get.show_name = b
+
+              label(class = "form-check-label"):
+                text "show name"
+
+            # has value
+            tdiv(class = "form-check form-switch"):
+              checkbox currentTag.get.hasValue, proc (b: bool) =
+                currentTag.get.value_type =
+                  if b: tvtStr
+                  else: tvtNone
+
+              label(class = "form-check-label"):
+                text "has value"
+
+            # value type
+            tdiv(class = "form-group my-2"):
+              label(class = "form-label"):
+                text "value type"
+
+              select(class = "form-select", disabled = not currentTag.get.hasValue):
+                for lbl in tvtStr..tvtJson:
+                  option(value = cstr lbl.ord, selected = currentTag.get.value_type == lbl):
+                    text $lbl
+
+                proc onInput(e: Event, v: Vnode) =
+                  currentTag.get.value_type = TagValueType parseInt e.target.value
+
+            # background
+            tdiv(class = "form-group d-inline-block mx-2"):
+              label(class = "form-check-label"):
+                text "background color: "
+
+              input(`type` = "color",
+                  class = "form-control",
+                  value = toColorString currentTag.get.theme.bg):
+                proc oninput(e: Event, v: Vnode) =
+                  currentTag.get.theme.bg = parseHexColorPack $e.target.value
+
+            # foreground
+            tdiv(class = "form-group d-inline-block mx-2"):
+              label(class = "form-check-label"):
+                text "foreground color: "
+
+              input(`type` = "color",
+                  class = "form-control",
+                  value = toColorString currentTag.get.theme.fg):
+                proc oninput(e: Event, v: Vnode) =
+                  currentTag.get.theme.fg = parseHexColorPack $e.target.value
+
+          tdiv(class = "my-2"):
+            tagViewC get currentTag, "val", noop
+
+          if selectedTagI == noIndex:
+            button(class = "btn btn-success w-100 mt-2 mb-4"):
+              text "add"
+              icon "mx-2 fa-plus"
 
               proc onclick =
-                state = asSelectIcon
+                apiCreateNewTag currentTag.get, proc =
+                  notify "tag created"
+                  waitAll [fetchTags()], proc = redraw()
 
-          tdiv(class = "form-check form-switch"):
-            checkbox currentTag.is_private, proc (b: bool) =
-              currentTag.is_private = b
+          else:
+            button(class = "btn btn-primary w-100 mt-2"):
+              text "update"
+              icon "mx-2 fa-sync"
 
-            label(class = "form-check-label"):
-              text "is private"
+              proc onclick =
+                apiUpdateTag get currentTag, proc =
+                  notify "tag updated"
+                  waitAll [fetchTags()], proc = redraw()
 
-          tdiv(class = "form-check form-switch"):
-            checkbox currentTag.show_name, proc (b: bool) =
-              currentTag.show_name = b
+            button(class = "btn btn-danger w-100 mt-2 mb-4"):
+              text "delete"
+              icon "mx-2 fa-trash"
 
-            label(class = "form-check-label"):
-              text "show name"
-
-          # has value
-          tdiv(class = "form-check form-switch"):
-            checkbox currentTag.hasValue, proc (b: bool) =
-              currentTag.value_type =
-                if b: tvtStr
-                else: tvtNone
-
-            label(class = "form-check-label"):
-              text "has value"
-
-          # value type
-          tdiv(class = "form-group my-2"):
-            label(class = "form-label"):
-              text "value type"
-
-            select(class = "form-select", disabled = not currentTag.hasValue):
-              for lbl in tvtStr..tvtJson:
-                option(value = cstr lbl.ord, selected = currentTag.value_type == lbl):
-                  text $lbl
-
-              proc onInput(e: Event, v: Vnode) =
-                currentTag.value_type = TagValueType parseInt e.target.value
-
-          # background
-          tdiv(class = "form-group d-inline-block mx-2"):
-            label(class = "form-check-label"):
-              text "background color: "
-
-            input(`type` = "color",
-                class = "form-control",
-                value = toColorString currentTag.theme.bg):
-              proc oninput(e: Event, v: Vnode) =
-                currentTag.theme.bg = parseHexColorPack $e.target.value
-
-          # foreground
-          tdiv(class = "form-group d-inline-block mx-2"):
-            label(class = "form-check-label"):
-              text "foreground color: "
-
-            input(`type` = "color",
-                class = "form-control",
-                value = toColorString currentTag.theme.fg):
-              proc oninput(e: Event, v: Vnode) =
-                currentTag.theme.fg = parseHexColorPack $e.target.value
-
-        tdiv(class = "my-2"):
-          tagViewC currentTag, "val", noop
-
-        if selectedTagI == noIndex:
-          button(class = "btn btn-success w-100 mt-2 mb-4"):
-            text "add"
-            icon "mx-2 fa-plus"
-
-            proc onclick =
-              apiCreateNewTag currentTag, proc =
-                notify "tag created"
-                waitAll [fetchTags()], proc = redraw()
-
-        else:
-          button(class = "btn btn-primary w-100 mt-2"):
-            text "update"
-            icon "mx-2 fa-sync"
-
-            proc onclick =
-              apiUpdateTag currentTag, proc =
-                notify "tag updated"
-                waitAll [fetchTags()], proc = redraw()
-
-          button(class = "btn btn-danger w-100 mt-2 mb-4"):
-            text "delete"
-            icon "mx-2 fa-trash"
-
-            proc onclick =
-              apiDeleteTag currentTag.id, proc =
-                notify "tag deleted"
-                waitAll [fetchTags()], proc = redraw()
-
+              proc onclick =
+                apiDeleteTag currentTag.get.id, proc =
+                  notify "tag deleted"
+                  waitAll [fetchTags()], proc = redraw()
 
 proc init* =
   setRenderer createDom
