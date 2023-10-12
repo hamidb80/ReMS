f82f654fafa5195da73a938ca0095daa5b2b8814
hamidb80 <hr.bolouri@gmail.com>
2023-10-12 17:20:17 +0330

    listen on 0.0.0.0

--- src/backend/server.nim
+++ src/backend/server.nim
@@ -9,4 +9,4 @@ import ../common/types
 proc runWebServer*(port: Port) {.noreturn.} =
   {.cast(gcsafe).}:
     var server = newServer(router, maxBodyLen = 5.Mb)
-    serve server, port
+    serve server, port, "0.0.0.0"
